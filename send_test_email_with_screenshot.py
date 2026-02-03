409cf2ddb26ed1fc872629c89e7aee6b0ed5b038
gelobias14 <gelobias14@gmail.com>
2026-02-03 20:17:11 +0800

    Update send_test_email_with_screenshot.py

--- send_test_email_with_screenshot.py
+++ send_test_email_with_screenshot.py
@@ -35,7 +35,7 @@ from selenium.webdriver.support import expected_conditions as EC
 DEFAULT_URLS: List[str] = [
     "https://www.youtube.com/watch?v=nw0tZFyExtY&list=RDnw0tZFyExtY&start_radio=1",
 ]
-DEFAULT_SELECTOR: str = "title"
+DEFAULT_SELECTOR: str = "h1"
 DEFAULT_EXPECTED_TEXT: str = "fitterkarma - Pag-Ibig ay Kanibalismo II (OFFICIAL MUKBANG VIDEO)"
 DEFAULT_TIMEOUT_MS: int = 15000
 DEFAULT_OUT_DIR: str = "screenshots"
